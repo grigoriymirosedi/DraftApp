6c6746042fff2d95d870c007eea6dae6b8f5f8d8
Grigoriy <grigoriymirosedi@gmail.com>
2022-09-29 22:52:37 +0300

    Обновлены иконки. Налажена реализация смены стиля иконки в зависимости от того, была ли она нажата или нет.
    Обновлены иконки фильтрации мусора. Теперь эти иконки помечаются как "выбранные" и "невыбранные".

--- app/src/main/java/com/example/googlerecyclemap/MainActivity.kt
+++ app/src/main/java/com/example/googlerecyclemap/MainActivity.kt
@@ -20,15 +20,36 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
 import com.google.android.gms.maps.model.Marker
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
 
-    private lateinit var PlasticFilterButton: ImageButton
-    private lateinit var GlassFilterButton: ImageButton
+    private lateinit var PlasticCard: CardView
+    private lateinit var PlasticImage: ImageView
 
-    private lateinit var Cards: CardView
+    private lateinit var GlassCard: CardView
+    private lateinit var GlassImage: ImageView
+
+    private lateinit var MetalCard: CardView
+    private lateinit var MetalImage: ImageView
+
+    private lateinit var LightCard: CardView
+    private lateinit var LightImage: ImageView
+
+    private lateinit var BatteryCard: CardView
+    private lateinit var BatteryImage: ImageView
+
+    private lateinit var ClothesCard: CardView
+    private lateinit var ClothesImage: ImageView
+
+    var plasticFlag = false;
+    var glassFlag = false;
+    var metalFlag = false;
+    var lightFlag = false;
+    var batteryFlag = false;
+    var clothesFlag = false;
 
     val rostov = LatLng(47.233, 39.700)
 
@@ -74,13 +95,104 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        Cards = findViewById(R.id.PlasticCard)
+        PlasticCard = findViewById(R.id.PlasticCard)
+        PlasticImage = findViewById(R.id.plasticImage)
 
-        Cards.setOnClickListener{
-            Log.d(TAG, "Plastic card has been clicked.")
+        GlassCard = findViewById(R.id.GlassCard)
+        GlassImage = findViewById(R.id.glassImage)
+
+        MetalCard = findViewById(R.id.MetalCard)
+        MetalImage = findViewById(R.id.metalImage)
+
+        LightCard = findViewById(R.id.LampCard)
+        LightImage = findViewById(R.id.lightImage)
+
+        BatteryCard = findViewById(R.id.BatteryCard)
+        BatteryImage = findViewById(R.id.batteryImage)
+
+        ClothesCard = findViewById(R.id.ClothesCard)
+        ClothesImage = findViewById(R.id.clothesImage)
+
+        PlasticCard.setOnClickListener{
+            if (plasticFlag == false){
+                Log.d(TAG, "Enable")
+                PlasticImage.setImageResource(R.drawable.is_enable_plastic_50)
+                plasticFlag = !plasticFlag
+            }
+            else{
+                Log.d(TAG, "Disable")
+                PlasticImage.setImageResource(R.drawable.is_disable_plastic_50)
+                plasticFlag = !plasticFlag
+            }
+        }
+
+        GlassCard.setOnClickListener{
+            if (glassFlag == false){
+                Log.d(TAG, "Enable")
+                GlassImage.setImageResource(R.drawable.is_enable_glass_50)
+                glassFlag = !glassFlag
+            }
+            else{
+                Log.d(TAG, "Disable")
+                GlassImage.setImageResource(R.drawable.is_disable_glass_50)
+                glassFlag = !glassFlag
+            }
+        }
+
+        MetalCard.setOnClickListener{
+            if (metalFlag == false){
+                Log.d(TAG, "Enable")
+                MetalImage.setImageResource(R.drawable.is_enable_metal_50)
+                metalFlag = !metalFlag
+            }
+            else{
+                Log.d(TAG, "Disable")
+                MetalImage.setImageResource(R.drawable.is_disable_metal_50)
+                metalFlag = !metalFlag
+            }
+        }
+
+        LightCard.setOnClickListener{
+            if (lightFlag == false){
+                Log.d(TAG, "Enable")
+                LightImage.setImageResource(R.drawable.is_enable_light_50)
+                lightFlag = !lightFlag
+            }
+            else{
+                Log.d(TAG, "Disable")
+                LightImage.setImageResource(R.drawable.is_disable_light_50)
+                lightFlag = !lightFlag
+            }
+        }
+
+        BatteryCard.setOnClickListener{
+            if (batteryFlag == false){
+                Log.d(TAG, "Enable")
+                BatteryImage.setImageResource(R.drawable.is_enable_battery_50)
+                batteryFlag = !batteryFlag
+            }
+            else{
+                Log.d(TAG, "Disable")
+                BatteryImage.setImageResource(R.drawable.is_disable_battery_50)
+                batteryFlag = !batteryFlag
+            }
+        }
+
+        ClothesCard.setOnClickListener{
+            if (clothesFlag == false){
+                Log.d(TAG, "Enable")
+                ClothesImage.setImageResource(R.drawable.is_enable_clothes_50)
+                clothesFlag = !clothesFlag
+            }
+            else{
+                Log.d(TAG, "Disable")
+                ClothesImage.setImageResource(R.drawable.is_disable_clothes_50)
+                clothesFlag = !clothesFlag
+            }
         }
 
     }
